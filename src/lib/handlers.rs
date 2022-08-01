2fb3cbc2280ad1b7183d0a5fb1cf8f5ebe4eef2f
itsezc <chirub@foretag.co>
2022-08-01 21:57:12 +0100

    implement(Instruction): ForwardHandller

--- src/lib/handlers.rs
+++ src/lib/handlers.rs
@@ -43,3 +43,42 @@ impl InstructionHandler for RightHandler {
 }
 
 pub struct ForwardHandler;
+
+impl InstructionHandler for ForwardHandler {
+	fn handle(&self, grid: &Grid, robot: &mut Robot, scents: &mut HashMap<String, bool>) {
+		// We ignore all the instructions if the robot is lost
+		if matches!(robot.status, RobotStatus::Lost) {
+			return;
+		}
+
+		let mut new_x: i8 = robot.last_position.x as i8;
+		let mut new_y: i8 = robot.last_position.y as i8;
+
+		match robot.last_position.orientation {
+			Orientation::North => new_y += 1,
+			Orientation::East => new_x += 1,
+			Orientation::South => new_y -= 1,
+			Orientation::West => new_x -= 1,
+		}
+
+		let scent_key = format!("{}-{}", new_x, new_y);
+
+		// If the processed new position is gonna be same as one contained on the scent hashmap
+		// we ignore the handling.
+		if scents.contains_key(&scent_key) {
+			return;
+		}
+
+		// The robot will fall
+		if new_x < 0 || new_x > grid.map.max_x as i8 || new_y < 0 || new_y > grid.map.max_y as i8 {
+			robot.status = RobotStatus::Lost;
+
+			scents.insert(scent_key, true);
+
+			return;
+		}
+
+		robot.last_position.x = new_x as u8;
+		robot.last_position.y = new_y as u8;
+	}
+}
